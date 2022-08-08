ed9f32c3827f09bbfea93d343b28882b28528626
LinhPham03 <phamthithuylinh190703@gmail.com>
2022-08-08 09:00:17 +0700

    version 1

---
+++ display-information.py
@@ -0,0 +1,6 @@
+from time import clock_settime
+print ("tên: Phạm Thị Thùy Linh")
+print ("ngày sinh: 19/07/03")
+print ("Địa chỉ: Quận Đống Đa, Hà NộI")
+print ("Mục đích đến với khóa học: học lập trình cơ bản để chuẩn bị cho công việc sau này")
+print ("kkk")
